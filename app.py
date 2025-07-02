9ca84d8a3803e20b23f308e8b664fe7bddd56d3d
malaika59 <malaikashoukat594@gmail.com>
2025-07-02 20:15:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import joblib
-
+import os
 # Debug: See current directory and files
 st.write("Current directory:", os.getcwd())
 st.write("Files in directory:", os.listdir())
